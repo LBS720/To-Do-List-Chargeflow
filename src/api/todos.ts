161e7758a75e5ad811ab74af64092c4fe74c8d4d
LBS <linoybs720@gmail.com>
2026-01-11 14:46:48 +0200

    insert env

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -4,6 +4,8 @@ import type { Todo } from "./types/todo";
 import type { TodosResponse } from "./types/todosResponse";
 import { apiRequest, API_BASE_URL } from "./apiClient";
 
+const TODOS_LIMIT = import.meta.env.TODOS_LIMIT || 100;
+
 export const fetchTodos = async (userId: number): Promise<Todo[]> => {
   const response = await apiRequest<TodosResponse>(
     `${API_BASE_URL}/todos/user/${userId}`
@@ -13,7 +15,7 @@ export const fetchTodos = async (userId: number): Promise<Todo[]> => {
 
 export const fetchAllTodos = async (): Promise<Todo[]> => {
   const response = await apiRequest<TodosResponse>(
-    `${API_BASE_URL}/todos?limit=100`
+    `${API_BASE_URL}/todos?limit=${TODOS_LIMIT}`
   );
   return response.todos;
 };
