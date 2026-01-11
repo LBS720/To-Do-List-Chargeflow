224f8d34b941f07a6b9889db12a4cc9915a5a616
LBS <linoybs720@gmail.com>
2026-01-11 15:43:46 +0200

    insert hooks to project

---
+++ src/hooks/useUsers.ts
@@ -0,0 +1,11 @@
+/** @format */
+
+import { useQuery } from "@tanstack/react-query";
+import { fetchUsers } from "@/api/users";
+
+export const useUsers = () => {
+  return useQuery({
+    queryKey: ["users"],
+    queryFn: fetchUsers,
+  });
+};
